10b7b55bbe0f81ccd499c89c1b07a783217839b5
PoeticDeath <46275713+PoeticDeath@users.noreply.github.com>
2021-06-15 20:59:19 -0400

    Update CUDARANDCompressor-CPU.py

--- CPU/CUDARANDCompressor-CPU.py
+++ CPU/CUDARANDCompressor-CPU.py
@@ -23,9 +23,7 @@ def CompressMT(a1, a2, a3, Threads, ANS, CUR, x):
             if int(nrandint(w, 0, 72057594037927936, 1)[0]).to_bytes(7, "big")[:len(a2[0])] == a2[0]:
                 strrec = nrandint(w, 0, 72057594037927936, int(a3))
                 strrec = [int(strrec[i]).to_bytes(7, "big") for i in range(0, len(strrec))]
-                if len(a2[-1]) % 7 != 0:
-                    while len(strrec[-1]) % 7 != len(a2[-1]) % 7:
-                        strrec[-1] = strrec[-1][:-1]
+                strrec[-1] = strrec[-1][:len(a2[-1])]
             else:
                 strrec = [0]
             n += 1
